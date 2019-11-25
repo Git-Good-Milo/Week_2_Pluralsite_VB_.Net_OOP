c94cb664a038f9c72865b8d2cdac2b19e4f8d03b
Miles <milotheengineer@gmail.com>
2019-11-25 15:38:43 +0000

    Finished OOP with VB .NET

--- vb_fundimentals_methods/MainModule.vb
+++ vb_fundimentals_methods/MainModule.vb
@@ -25,6 +25,32 @@
         ' Notice how an instace of the class was not needed
         Console.WriteLine(Product.CalculateTheProfit(900, 1400))
 
+        ' Code to show Product Class inheriting from CommonBase Class
+        Dim prod2 As New Product
+        prod2.IsActive = True
+        prod2.CreatedBy = "Man like Milo"
+        ' The methods and fucntions from Product Class have not been affected by the inheritance 
+        ' and can still be used
+        prod2.ListPrice = 600
+
+        ' Code to Show how a With statement can be used in conjuntion with a class during inheritance
+        ' The With Key word allows us to chain properties in a Class without having to referecnce thre variable name
+        Dim prod3 As New Product With {
+            .ProductID = 700,
+            .Name = "10 Speed Bike",
+            .ProductNumber = "12d-dja8-a"
+        }
+        Console.WriteLine(prod3.ToString())
+
+        Dim cust As New Customer With {
+            .CustomerID = 1,
+            .CompanyName = "Beach Computer Consulting",
+            .FirstName = "Bruce",
+            .LastName = "Jones"
+        }
+        Console.WriteLine(cust.ToString())
+
+
         Console.ReadKey()
     End Sub
 
--- vb_fundimentals_methods/Product.vb
+++ vb_fundimentals_methods/Product.vb
@@ -1,4 +1,7 @@
 ﻿Public Class Product
+    ' This Product Class inherits from the CommonBase Class
+    Inherits CommonBase
+
     ' This is how you can initialse variables with a constructor.
     ' By intialising you can set the default values for variables in a class.
     Sub New()
@@ -60,13 +63,29 @@
 
 
 
-    ' This is a function to demonstarte optional methods
-    'Function CalculateProfit(Optional ByVal newCost As Decimal = 0) As Decimal
-    '    If newCost <> 0 Then
-    '        StandardCost = newCost
-    '    End If
+    'This Is a function to demonstarte optional methods
+    Function CalculateProfit2(Optional ByVal newCost As Decimal = 0) As Decimal
+        If newCost <> 0 Then
+            StandardCost = newCost
+        End If
 
-    '    Return ListPrice - StandardCost
-    'End Function
+        Return ListPrice - StandardCost
+    End Function
+
+    ' This function utilises inheritance from the CommonBase Class
+    ' The overides keyword here allows us to reduce the amount of code we use by using MyBase.GetClassData
+    ' This gets from the overridible function on CommonBase.
+    ' The same is done on the Customer Class
+    Protected Overrides Function GetClassData() As String
+        Dim sb As New Text.StringBuilder(1024)
+
+        sb.AppendLine("Product ID: " + ProductID.ToString)
+        sb.AppendLine("Product Name: " + Name)
+        sb.AppendLine("Product Number: " + ProductNumber)
+        sb.AppendLine(MyBase.GetClassData())
+
+        Return sb.ToString()
+
+    End Function
 
 End Class
